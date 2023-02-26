019efdbc38312f09264d347f550006a617cbd300
KipronohVincent <kipronohvincent43@gmail.com>
2023-02-26 11:38:36 +0300

    The Client API

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -24,4 +24,29 @@ defmodule KV.Registry do
       {:noreply, Map.put(names, name, bucket)}
     end
   end
+
+  ## Client API
+
+  @doc """
+  Starts the registry.
+  """
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  @doc """
+  Looks up the bucket pid for `name` stored in `server`.
+
+  Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
+  """
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
+  @doc """
+  Ensures there is a bucket associated with the given `name` in `server`.
+  """
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
 end
